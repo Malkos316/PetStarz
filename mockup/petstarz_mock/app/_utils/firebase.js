02d2bba2f4c95de312c228cc9ac51b5a76536aa0
Sam <samantha.bogen@edu.sait.ca>
2023-11-28 14:03:28 -0700

    missed something

--- mockup/petstarz_mock/app/_utils/firebase.js
+++ mockup/petstarz_mock/app/_utils/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth } from "firebase/auth";
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,18 +18,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-const provider = new GoogleAuthProvider();
-
-export const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-
-        console.log(result);
-    }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
-        console.log(errorCode, errorMessage, email, credential);
-    });
-
-};
